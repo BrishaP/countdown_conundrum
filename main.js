c19a4858e4739e73be07c9eabac626fb2ae614fe
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-09-10 22:58:03 +0100

    making public version of kata

---
+++ main.js
@@ -0,0 +1,27 @@
+//Level 1
+
+/**
+"PLEASE HELP! I told Carol I would fill in for her at work, but I don't know the differences between vowels and consonants... 
+Can you help me by writing a function?"
+
+Your function should ONLY take in a string containing a single letter in a string either uppercase or lowercase.
+
+When you are ready to check your function, use the npm run test command in the terminal!
+
+A few examples 
+
+ 'function consonantOrVowel('A')' should return the string 'Vowel'
+
+  'function consonantOrVowel('b')' should return the string 'Consonant'
+
+  function  'function consonantOrVowel('33')' should return the string 'Not a valid input, please enter a single letter string'.
+
+  **/
+
+
+  export function consonantOrVowel(letter){
+    //Please enter your code here
+    //Good luck!
+    };
+    
+      
